897c34364106705307b5825d3406c87b8abe4435
kimholmgren <kimberly.m.holmgren@gmail.com>
2016-08-09 10:35:17 -0700

    changed path in build, removed extra version of redisurl

--- tomcat/webapps/googlepoodle/WEB-INF/build.bat
+++ tomcat/webapps/googlepoodle/WEB-INF/build.bat
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CLASSPATH=./classes/hamcrest-core-1.3.jar:./classes/hamcrest-library-1.3.jar:./classes/jedis-2.8.0.jar:./classes/jsoup-1.8.3.jar:./classes/junit-4.12.jar:.:/Applications/tomcat/lib/servlet-api.jar
+CLASSPATH=./classes/hamcrest-core-1.3.jar:./classes/hamcrest-library-1.3.jar:./classes/jedis-2.8.0.jar:./classes/jsoup-1.8.3.jar:./classes/junit-4.12.jar:.:./../../../lib/servlet-api.jar
 javac -cp $CLASSPATH classes/*.java
